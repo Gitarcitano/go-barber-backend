8f1770629623b5411d95402d0235ff5a88a513d3
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2020-06-09 11:13:06 -0300

    finished unit tests

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -31,7 +31,7 @@ describe('CreateAppointment', () => {
       provider_id: '12121212',
     });
 
-    expect(
+    await expect(
       createAppointment.execute({
         date: appointmentDate,
         provider_id: '12121212',
