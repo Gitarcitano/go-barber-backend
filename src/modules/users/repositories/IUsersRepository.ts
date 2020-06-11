5efcc10077d9c59816b97d783b1c27feed30f7c1
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2020-06-10 23:36:13 -0300

    add Provider services

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,7 +1,9 @@
 import User from '../infra/typeorm/entities/User';
 import ICreateuserDTO from '../dtos/ICreateUserDTO';
+import IFindAllProvidersDTO from '../dtos/IFindAllProvidersDTO';
 
 export default interface IUsersRepository {
+  findAllProviders(data: IFindAllProvidersDTO): Promise<User[]>;
   findById(id: string): Promise<User | undefined>;
   findByEmail(email: string): Promise<User | undefined>;
   create(data: ICreateuserDTO): Promise<User>;
