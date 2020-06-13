1080a85b006c0c75e4cd58340cc0fff9d92970bd
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2020-06-12 22:42:02 -0300

    add SES AWs Mail Provider

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'lalalalal@giovanne.com.br',
+      name: 'Giovanne O sem email',
+    },
+  },
+} as IMailConfig;
