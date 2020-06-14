004ee00b2cb9a497f45bc04513294f161ff5558c
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2020-06-13 21:43:41 -0300

    added relation users in appointments

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -62,6 +62,7 @@ class AppointmensRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
